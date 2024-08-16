d16852126ff0a68601385708c61f444bcf8ac80e
MajorNameless <rtpeducation@gmail.com>
2024-08-16 17:33:19 +0200

    Added IVehicles

---
+++ ConsoleApp1/IVehicles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Vehicles
+{
+    internal interface IVehicles
+    {
+        string Make { get; set; }
+        string Model { get; set; }
+        int Year { get; set; }
+        double LoadCapacity { get; set; }
+        string NumberPlate { get; set; }
+        string Status { get; set; }
+    }
+}
