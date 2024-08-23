8ce0bc790019746e6a6429a0f0e97f0de9fb2bfa
zannlin <zanderlindeque1@gmail.com>
2024-08-23 20:44:29 +0200

    Added stuff in trucks

--- ConsoleApp1/Classes/Truck.cs
+++ ConsoleApp1/Classes/Truck.cs
@@ -21,6 +21,42 @@ namespace ConsoleApp1.Classes
             set { _LoadCapacity = value; }
         }
 
+        public override bool AddVehicle(Vehicles vehicle) //returns a bool to determine if the numberplate is unique or not
+        {
+            bool NotUnique = false;
+            foreach (Vehicles veci in VehicleList)
+            {
+                if (veci.NumberPlate == vehicle.NumberPlate)
+                {
+                    NotUnique = true;
+                }
+            }
+            if (!NotUnique)
+            {
+                bool validated = false;
+                double LoadCap = 0;
+                while (validated == false)
+                {
+                    Console.Write("Enter the load capacity: ");
+                    validated = double.TryParse(Console.ReadLine(), out LoadCap);
+                    if (!validated)
+                    {
+                        Console.WriteLine("\nInput invalid, please try again\n");
+                    }
+                }
+
+                Truck truck = new Truck(vehicle.Make, vehicle.Model, vehicle.Year, vehicle.NumberPlate, vehicle.Status, vehicle.Availability, LoadCap);
+                VehicleList.Add(truck);
+                Console.WriteLine($"\n{vehicle.Make} {vehicle.Model} with number plate {vehicle.NumberPlate} added successfully!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("\nThe numberplate entered is not unique" +
+                    $"{vehicle.Make} {vehicle.Model} with number plate {vehicle.NumberPlate} not added\n");
+                return false;
+            }
+        }
 
     }
 }
